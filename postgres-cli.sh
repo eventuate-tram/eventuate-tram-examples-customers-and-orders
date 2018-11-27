22744148be91f2417fb5e35042237778f9a129ee
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-11-27 15:48:18 +0300

    Updated eventuate tram, docker-compose, fixed mysql/postgres cli.

--- postgres-cli.sh
+++ postgres-cli.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
 
 docker run $* \
-   --name postgresterm --rm \
-   -e POSTGRES_ENV_POSTGRES_USER=eventuate -e POSTGRES_ENV_POSTGRES_PASSWORD=eventuate -e POSTGRES_HOST=$DOCKER_HOST_IP \
-   postgres:9.6.5 \
-   sh -c 'export PGPASSWORD="$POSTGRES_ENV_POSTGRES_PASSWORD"; exec psql -h $POSTGRES_HOST -U "$POSTGRES_ENV_POSTGRES_USER" '
+   --name postgresterm \
+   -e POSTGRES_HOST=$DOCKER_HOST_IP \
+   --rm postgres:9.6.5 \
+   sh -c 'export PGPASSWORD=eventuate; exec psql -h "$POSTGRES_HOST" -U eventuate'
