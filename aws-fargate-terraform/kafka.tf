49a3ad0ea158552dbbca9307c51a3518c5885ec9
Chris Richardson <chris@chrisrichardson.net>
2019-07-11 11:04:05 -0700

    Enabled Plain text communication

--- aws-fargate-terraform/kafka.tf
+++ aws-fargate-terraform/kafka.tf
@@ -20,7 +20,7 @@ resource "aws_msk_cluster" "eventuate" {
     encryption_at_rest_kms_key_arn = "${aws_kms_key.kms.arn}"
 
     encryption_in_transit = {
-      client_broker = "TLS"
+      client_broker = "TLS_PLAINTEXT"
     }
   }
 
