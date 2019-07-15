faf39459d7e74f8dcec7ebb349e0ebacf7e59ed3
Chris Richardson <chris@chrisrichardson.net>
2019-07-15 09:21:11 -0700

    Fixes #16 - retry upserts

--- order-history-backend/src/main/java/io/eventuate/examples/tram/ordersandcustomers/orderhistory/backend/OrderHistoryViewMongoConfiguration.java
+++ order-history-backend/src/main/java/io/eventuate/examples/tram/ordersandcustomers/orderhistory/backend/OrderHistoryViewMongoConfiguration.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
 @EnableMongoRepositories
+@EnableRetry
 public class OrderHistoryViewMongoConfiguration {
 
   @Bean
--- order-history-backend/src/main/java/io/eventuate/examples/tram/ordersandcustomers/orderhistory/backend/OrderHistoryViewService.java
+++ order-history-backend/src/main/java/io/eventuate/examples/tram/ordersandcustomers/orderhistory/backend/OrderHistoryViewService.java
@@ -3,7 +3,10 @@ package io.eventuate.examples.tram.ordersandcustomers.orderhistory.backend;
 import io.eventuate.examples.tram.ordersandcustomers.commondomain.Money;
 import io.eventuate.examples.tram.ordersandcustomers.commondomain.OrderState;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 
 public class OrderHistoryViewService {
 
@@ -20,24 +23,44 @@ public class OrderHistoryViewService {
     customerViewRepository.addCustomer(customerId, customerName, creditLimit);
   }
 
+  @Retryable(
+          value = { DuplicateKeyException.class },
+          maxAttempts = 4,
+          backoff = @Backoff(delay = 250))
   public void addOrder(Long customerId, Long orderId, Money orderTotal) {
     customerViewRepository.addOrder(customerId, orderId, orderTotal);
     orderViewRepository.addOrder(orderId, orderTotal);
   }
 
+  @Retryable(
+          value = { DuplicateKeyException.class },
+          maxAttempts = 4,
+          backoff = @Backoff(delay = 250))
   public void approveOrder(Long customerId, Long orderId) {
     updateOrderState(customerId, orderId, OrderState.APPROVED);
   }
 
+  @Retryable(
+          value = { DuplicateKeyException.class },
+          maxAttempts = 4,
+          backoff = @Backoff(delay = 250))
   private void updateOrderState(Long customerId, Long orderId, OrderState state) {
     customerViewRepository.updateOrderState(customerId, orderId, state);
     orderViewRepository.updateOrderState(orderId, state);
   }
 
+  @Retryable(
+          value = { DuplicateKeyException.class },
+          maxAttempts = 4,
+          backoff = @Backoff(delay = 250))
   public void rejectOrder(Long customerId, Long orderId) {
     updateOrderState(customerId, orderId, OrderState.REJECTED);
   }
 
+  @Retryable(
+          value = { DuplicateKeyException.class },
+          maxAttempts = 4,
+          backoff = @Backoff(delay = 250))
   public void cancelOrder(Long customerId, long orderId) {
     updateOrderState(customerId, orderId, OrderState.CANCELLED);
   }
