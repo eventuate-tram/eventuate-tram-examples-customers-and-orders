b604d5b09be9f0fb2640884496df83d9cb4740a8
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-12-16 11:39:28 +0300

    Fixed jmeter test plan configurator.

--- jmeter-service/src/main/java/io/eventuate/examples/tram/ordersandcustomers/jmeter/TestPlanConfigurator.java
+++ jmeter-service/src/main/java/io/eventuate/examples/tram/ordersandcustomers/jmeter/TestPlanConfigurator.java
@@ -6,20 +6,22 @@ import java.util.List;
 
 public class TestPlanConfigurator {
 
-  private static final String LOOPS_TEMPLATE = "<stringProp name=\"LoopController.loops\">%s</stringProp>";
-  private static final String THREADS_TEMPLATE = "<stringProp name=\"ThreadGroup.num_threads\">%s</stringProp>";
-  private static final String RAMP_TIME_TEMPLATE = "<stringProp name=\"ThreadGroup.ramp_time\">%s</stringProp>";
-  private static final String DOMAIN_TEMPLATE = "<stringProp name=\"HTTPSampler.domain\">%s</stringProp>";
-  private static final String PORT_TEMPLATE = "<stringProp name=\"HTTPSampler.port\">%s</stringProp>";
+  private static final String PROPERTY_TEMPLATE = "<stringProp name=\"%s\">%s</stringProp>";
 
-  private static final List<String> TEMPLATES = Arrays.asList(LOOPS_TEMPLATE, THREADS_TEMPLATE, RAMP_TIME_TEMPLATE, DOMAIN_TEMPLATE, PORT_TEMPLATE);
+  private static final String LOOPS_PROPERTY = "LoopController.loops";
+  private static final String THREADS_PROPERTY = "ThreadGroup.num_threads";
+  private static final String RAMP_TIME_PROPERTY = "ThreadGroup.ramp_time";
+  private static final String DOMAIN_PROPERTY = "HTTPSampler.domain";
+  private static final String PORT_PROPERTY = "HTTPSampler.port";
+
+  private static final List<String> PROPERTIES = Arrays.asList(LOOPS_PROPERTY, THREADS_PROPERTY, RAMP_TIME_PROPERTY, DOMAIN_PROPERTY, PORT_PROPERTY);
 
   public String configure(String testPlan, int loops, int threads, int rampTime, String domain, int port) {
-    List<Object> templateParameters = Arrays.asList(loops, threads, rampTime, domain, port);
+    List<Object> propertyValues = Arrays.asList(loops, threads, rampTime, domain, port);
 
-    for (int i = 0; i < TEMPLATES.size(); i++) {
-      String template = TEMPLATES.get(i);
-      testPlan = testPlan.replaceAll(String.format(template, "(.*)"), String.format(template, templateParameters.get(i)));
+    for (int i = 0; i < PROPERTIES.size(); i++) {
+      String property = PROPERTIES.get(i);
+      testPlan = testPlan.replaceAll(String.format(PROPERTY_TEMPLATE, property, "(.*)"), String.format(PROPERTY_TEMPLATE, property, propertyValues.get(i)));
     }
 
     return testPlan;
