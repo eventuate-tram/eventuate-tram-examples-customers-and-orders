2306e9084bce0076771ac5a81262c0c5e9b0aa40
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-04-24 16:26:39 +0300

    Updated java version on circleci

--- .circleci/install-jdk-14.sh
+++ .circleci/install-jdk-14.sh
@@ -3,6 +3,6 @@
 export SDKMAN_DIR=/home/circleci/.sdkman
 curl -s "https://get.sdkman.io" | bash
 source "$SDKMAN_DIR/bin/sdkman-init.sh"
-sdk install java 14.0.0-open
+sdk install java 14.0.1-open
 # Repeat to get env var set
 source "$SDKMAN_DIR/bin/sdkman-init.sh"
