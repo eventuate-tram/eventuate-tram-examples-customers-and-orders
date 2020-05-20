44f7f92551476eb6d9d5a858bd1e642a5fdc91a6
Chris Richardson <chris@chrisrichardson.net>
2020-05-20 07:24:10 -0700

    Improved capture of container logs

--- .circleci/print-container-logs.sh
+++ .circleci/print-container-logs.sh
@@ -5,9 +5,16 @@ CONTAINER_IDS=$(docker ps -a -q)
 for id in $CONTAINER_IDS ; do
   echo "\n--------------------"
   echo "logs of:\n"
-  docker ps -a -f "id=$id" 	
+  docker ps -a -f "id=$id"
   echo "\n"
   docker logs $id
   echo "--------------------\n"
 done
 
+mkdir -p ~/container-logs
+
+docker ps -a > ~/container-logs/containers.txt
+
+for name in $(docker ps -a --format "{{.Names}}") ; do
+  docker logs $name > ~/container-logs/${name}.log
+done
