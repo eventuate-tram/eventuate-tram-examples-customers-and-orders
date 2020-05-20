601cae48da95325fc269fb85079ca81f54c44951
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-05-20 18:17:27 +0300

    Fixed snapshot tests. Limited memory of java services.

--- build-and-test-snapshots.sh
+++ build-and-test-snapshots.sh
@@ -4,13 +4,15 @@ set -e
 
 dockerall="./gradlew  mysqlbinlogtextsearchCompose"
 dockerinfrastructure="./gradlew mysqlbinloginfrastructuretextsearchCompose"
+dockercustomersandorders="./gradlew mysqlbinlogtextsearchcustomersandordersCompose"
+
 
 ${dockerall}Down
 ${dockerinfrastructure}Up
 
 ./gradlew assemble
 
-${dockerall}Up
+${dockercustomersandorders}Up
 
 ./gradlew :snapshot-tests:cleanTest :snapshot-tests:test
 
