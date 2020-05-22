79e80756a185c129ff0f2bfa82acdd0d3b191404
Chris Richardson <chris@chrisrichardson.net>
2020-05-22 13:11:18 -0700

    Run docker-compose after upgrading

--- .circleci/upgrade-docker-compose.sh
+++ .circleci/upgrade-docker-compose.sh
@@ -2,6 +2,9 @@
 
 docker-compose version
 docker version
-curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` > ~/docker-compose
+URL="https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m`"
+echo Downloading from $URL
+curl -L $URL > ~/docker-compose
 chmod +x ~/docker-compose
 sudo mv ~/docker-compose /usr/local/bin/docker-compose
+docker-compose version
