f943233e0d578ac1a709371d7f3197bd1433fc40
Chris Richardson <chris@chrisrichardson.net>
2020-05-25 09:26:10 -0700

    Build needed container images before pushing

--- publish-docker-images.sh
+++ publish-docker-images.sh
@@ -6,7 +6,7 @@ export DOCKER_IMAGE_TAG=latest
 
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
-./gradlew assemble mysqlbinlogwithjmeterComposeBuild mysqlbinlogwithjmeterComposePush
+./gradlew assemble mysqlbinlogComposeBuild mysqlbinlogwithjmeterComposeBuild mysqlbinlogwithjmeterComposePush
 
 ./gradlew javaDevelopmentComposePull || echo no image to pull
 
