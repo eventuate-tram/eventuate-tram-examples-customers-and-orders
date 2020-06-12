e730a41bcaaea1f54e057518b4cd9abadbc921f4
Chris Richardson <chris@chrisrichardson.net>
2020-06-12 11:44:58 -0700

    Merged order-service-backend into order-service

--- build-and-test-all-mysql-maven.sh
+++ build-and-test-all-mysql-maven.sh
@@ -1,11 +1,11 @@
-#! /bin/bash
+#! /bin/bash -e
 
 export EVENTUATE_COMMON_VERSION=0.9.0.RELEASE
 export EVENTUATE_KAFKA_VERSION=0.3.0.RELEASE
 export EVENTUATE_CDC_VERSION=0.6.1.RELEASE
 export EVENTUATE_JAVA_BASE_IMAGE_VERSION=BUILD-5
 
-./mvnw package test-compile -DskipTests
+./mvnw test-compile package -DskipTests
 
 docker-compose -f docker-compose-mysql-binlog-maven.yml down
 docker-compose -f docker-compose-mysql-binlog-maven.yml up --build -d mysql zookeeper kafka
