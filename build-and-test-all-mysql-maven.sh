bfaa3c778730ed567ec47a0638fd3b8fa6e96b1a
Chris Richardson <chris@chrisrichardson.net>
2020-12-01 07:17:56 -0800

    Fixes #74 Replaced SNAPSHOT with RELEASE, Fixed issue with pom.xml dependencies, Maven build now runs the tests

--- build-and-test-all-mysql-maven.sh
+++ build-and-test-all-mysql-maven.sh
@@ -4,31 +4,30 @@ set -e
 
 java -version
 
-source ./.circleci/install-jdk-14.sh
+if [ -z "$SKIP_INSTALL_JDK_14" ] ; then
+  source ./.circleci/install-jdk-14.sh
+fi
 
 java -version
 
-export EVENTUATE_COMMON_VERSION=0.8.0.RELEASE
-export EVENTUATE_KAFKA_VERSION=0.3.0.RELEASE
+export EVENTUATE_COMMON_VERSION=0.9.0.RELEASE
 export EVENTUATE_CDC_VERSION=0.6.1.RELEASE
 export EVENTUATE_JAVA_BASE_IMAGE_VERSION=BUILD-5
 
 ./mvnw package -DskipTests
 
 docker-compose -f docker-compose-mysql-binlog-maven.yml down
-docker-compose -f docker-compose-mysql-binlog-maven.yml up --build -d mysql zookeeper kafka
 
-./wait-for-mysql.sh
-
-docker-compose -f docker-compose-mysql-binlog-maven.yml up --build -d cdcservice
+docker-compose -f docker-compose-mysql-binlog-maven.yml up --build -d cdcservice mongodb
 
 ./wait-for-services.sh ${DOCKER_HOST_IP:-localhost} "8099"
+
+./mvnw -am -pl '!end-to-end-tests' package
 
 docker-compose -f docker-compose-mysql-binlog-maven.yml up --build -d
 
 ./wait-for-services.sh ${DOCKER_HOST_IP:-localhost} "8081 8082 8083"
 
-
-./mvnw -am -pl order-history-service test-compile
+./mvnw -pl end-to-end-tests test
 
 docker-compose -f docker-compose-mysql-binlog-maven.yml down
