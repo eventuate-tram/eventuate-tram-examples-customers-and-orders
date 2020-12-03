cbd32dc4cacf53f2856b5b3acc8141295e8688fb
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-12-03 17:37:47 +0300

    Removed db id migration files, replaced by downloading using curl. Replaced mssql db id migration by direct docker-compose call. Added migration verification.

--- migration_scripts/mssql/entrypoint.sh
+++ migration_scripts/mssql/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/mssql-tools/bin/sqlcmd -S $TRAM_DB_SERVER -U sa -P $TRAM_SA_PASSWORD -b -d $TRAM_DB -I -i add-database-id-support-to-eventuate-mssql.migration.sql || exit 1
+/opt/mssql-tools/bin/sqlcmd -S $TRAM_DB_SERVER -U sa -P $TRAM_SA_PASSWORD -b -d $TRAM_DB -I -i migration.sql || exit 1
