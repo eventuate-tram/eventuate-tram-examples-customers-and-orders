3d925d6b2d303d469b729d2e6f2a7270bac83902
Chris Richardson <chris@chrisrichardson.net>
2022-05-20 13:00:14 +0900

    Defined extra env vars in _set-image-version-env-vars.sh and refactored

--- _set-image-version-env-vars.sh
+++ _set-image-version-env-vars.sh
@@ -1,6 +1,13 @@
 
-export EVENTUATE_MESSAGING_KAFKA_IMAGE_VERSION=$(sed -e '/^eventuateMessagingKafkaImageVersion=/!d' -e 's/eventuateMessagingKafkaImageVersion=//' < gradle.properties)
-export EVENTUATE_COMMON_VERSION=$(sed -e '/^eventuateCommonImageVersion=/!d' -e 's/eventuateCommonImageVersion=//' < gradle.properties)
-export EVENTUATE_CDC_VERSION=$(sed -e '/^eventuateCdcImageVersion=/!d' -e 's/eventuateCdcImageVersion=//' < gradle.properties)
-export EVENTUATE_JAVA_BASE_IMAGE_VERSION=$(sed -e '/^eventuateExamplesBaseImageVersion=/!d' -e 's/eventuateExamplesBaseImageVersion=//' < gradle.properties)
+property_value() {
+  property=${1?}
+  sed -e "/^$property=/!d" -e "s/$property=//" < gradle.properties
+}
+
+export JAR_DIR=target
+export SERVICE_IMAGE_VERSION=$(property_value version)
+export EVENTUATE_MESSAGING_KAFKA_IMAGE_VERSION=$(property_value eventuateMessagingKafkaImageVersion)
+export EVENTUATE_COMMON_VERSION=$(property_value eventuateCommonImageVersion)
+export EVENTUATE_CDC_VERSION=$(property_value eventuateCdcImageVersion)
+export EVENTUATE_JAVA_BASE_IMAGE_VERSION=$(property_value eventuateExamplesBaseImageVersion)
 
