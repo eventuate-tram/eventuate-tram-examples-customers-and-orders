72983ed3fa2a4c3bf07e1df11f355bc589a492b4
Chris Richardson <chris@chrisrichardson.net>
2022-05-20 13:09:35 +0900

    Removed JAR_DIR from _set-image-version-env-vars.sh

--- _set-image-version-env-vars.sh
+++ _set-image-version-env-vars.sh
@@ -4,7 +4,6 @@ property_value() {
   sed -e "/^$property=/!d" -e "s/$property=//" < gradle.properties
 }
 
-export JAR_DIR=target
 export SERVICE_IMAGE_VERSION=$(property_value version)
 export EVENTUATE_MESSAGING_KAFKA_IMAGE_VERSION=$(property_value eventuateMessagingKafkaImageVersion)
 export EVENTUATE_COMMON_VERSION=$(property_value eventuateCommonImageVersion)
