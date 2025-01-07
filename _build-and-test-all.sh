a1a45ffd9205b45cc34b0f622da11b1c0790ba03
Chris Richardson <chris@chrisrichardson.net>
2025-01-07 14:10:08 +0900

    Removed Docker Compose and removed image publishing

--- _build-and-test-all.sh
+++ _build-and-test-all.sh
@@ -2,35 +2,6 @@
 
 set -e
 
-dockerall="./gradlew ${DATABASE?}${MODE?}Compose"
-dockerinfrastructure="./gradlew ${DATABASE?}${MODE?}infrastructureCompose"
-
-./gradlew testClasses
-
-${dockerall}Down -P removeContainers=true
-${dockerinfrastructure}Up
-
-./gradlew -x :end-to-end-tests:test build
-
-#Testing db cli
-if [ "${DATABASE}" == "mysql" ]; then
-  echo 'show databases;' | ./mysql-cli.sh -i
-elif [ "${DATABASE}" == "postgres" ]; then
-  echo '\l' | ./postgres-cli.sh -i
-elif [ "${DATABASE}" == "mssql" ]; then
-  ./mssql-cli.sh "SELECT name FROM master.sys.databases;"
-else
-  echo "Unknown Database"
-  exit 99
-fi
+./gradlew build
 
 
-${dockerall}Build
-${dockerall}Up
-
-#Testing mongo cli
-echo 'show dbs' |  ./mongodb-cli.sh -i
-
-./gradlew :end-to-end-tests:cleanTest :end-to-end-tests:test
-
-${dockerall}Down -P removeContainers=true
